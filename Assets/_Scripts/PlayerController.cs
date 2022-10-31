291bf370463b2c24185804ab45d4d895986dd484
Jared Slawski <u1306392@utah.edu>
2022-10-30 23:32:39 -0600

    Checkpoint system

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -26,6 +26,8 @@ public class PlayerController : MonoBehaviour
 
     public PlayerSkills skills;
 
+    public Vector3 latestCheckpointPosition = Vector3.zero;
+
     private void Start()
     {
         this.playerRb = GetComponent<Rigidbody>();
@@ -41,6 +43,11 @@ public class PlayerController : MonoBehaviour
     private void Update()
     {
         this.currentState.UpdateState();
+
+        if (Input.GetKeyUp(KeyCode.R))
+        {
+            this.Respawn();
+        }
     }
 
     private void FixedUpdate()
@@ -67,6 +74,12 @@ public class PlayerController : MonoBehaviour
         this.debugStateName = this.currentState.GetType().ToString();
     }
 
+    public void Respawn()
+    {
+        this.playerRb.position = this.latestCheckpointPosition;
+        this.ChangeState(new FallState());
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Water")
@@ -79,6 +92,11 @@ public class PlayerController : MonoBehaviour
             SkillPickup newSkill = other.gameObject.GetComponent<SkillPickup>();
             this.skills.AddSkill(newSkill.skillStateName);
             Destroy(other.gameObject);
+        }
+
+        if (other.tag == "Checkpoint")
+        {
+            this.latestCheckpointPosition = other.bounds.center;
         }
     }
 
